8d609141fa52cabacb3921f6aa076145c291fef5
f3nett <f3nett@yandex.ru>
2023-02-08 22:40:02 +0300

    add templates

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    template = 'posts/index.html'
+    title = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title,
+    }
+    return render(request, template, context) 
 
-# Страница со списком мороженого
-def group_posts(request, slug):
-    return HttpResponse(f'Пост {slug}')
+def group_posts(request):
+    template = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title,
+    }
+    return render(request, template, context)
