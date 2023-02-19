7fa95f3acbda1ad9adb143f02231b056fbb36359
f3nett <f3nett@yandex.ru>
2023-02-19 20:48:56 +0300

    Fix errors

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Group(models.Model):
-    title = models.TextField()
-    slug = models.SlugField()
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
     description = models.TextField()
 
     def __str__(self):
